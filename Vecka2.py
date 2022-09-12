2fc8cc23a18530942eea097f6296b48c8350e954
Steven Lomon Lennartsson <steven.lennartsson@gmail.com>
2022-09-12 11:26:21 +0200

    chess board existent

--- Vecka2.py
+++ Vecka2.py
@@ -1,9 +1,10 @@
 #YH Grundläggande Python Vecka 2 labbar
-#while not finished
+#while True: infinite loop with a break is suuuuper common
+#can use pass as a temporary placeholder!
 from dataclasses import replace
 import random
 import time
-from tkinter import Y
+import termcolor
 
 #LOOPAR
 # for i in range(11):
@@ -54,10 +55,8 @@ from tkinter import Y
 
 #LISTS
 # numbers = []
-# numbers.append(int(input("Mata in tal#1: ")))
-# numbers.append(int(input("Mata in tal#2: ")))
-# numbers.append(int(input("Mata in tal#3: ")))
-# numbers.append(int(input("Mata in tal#4: ")))
+# for x in range(0,4):
+#     numbers.append(int(input(f"Mata in tal#{x+1}: ")))
 # print(numbers)
 # numbers.sort()
 # print(f"Största numret är {numbers[-1]}") #list[-1] is SUPER USEFUL!!
@@ -104,9 +103,12 @@ from tkinter import Y
 
 board = []
 for i in range(8):
-    row = [' ' for i in range(8)]
+    row = [f"{[i]}{[j]}" for j in range(8)] #This is the same as having two nested for loops
     board.append(row)
 
+print(f"{board}\n")
+board[1:8] = ["   b","  b","  b","  b","  b","  b","  b"]
+#board[6:13] = ["  b"," b"," b"," b"," b"," b"," b"]
 print(board)
 
 #STRINGS
@@ -116,35 +118,70 @@ print(board)
 # print(a+b+c)
 
 # hello = "Hello, world"
-# def countPosition(word):
-#     counter = 0
-#     for x in hello:
-#         counter += 1
-#         if x == "w":
-#             break
-#     return counter
+# def findPosition(word, letter):
+#     letterPos = word.find(letter)
+#     return letterPos
 
 # print(f"Hello = {hello}")
-# print(f"w is at position {countPosition(hello)}")
+# print(f"w is at position {findPosition(hello, 'w')}")
 
 # namn = "kurt andersson"
 # def capitalizeNames(full_name):
-#     full_name.upper() #Capitalize first name
-#     for x in full_name:
-#         if x == " ":
-#             WHAT GOES HERE???
-#             print("Hej")
-#     return full_name
+#     capitalized_name = ""
+#     firstLetter = True
+#     prevLetter = ""
+#     #full_name[0] = full_name[0].upper()
+#     #space_pos = full_name.find(" ")
+#     for letter in full_name:
+#         if firstLetter == True or prevLetter == " ":
+#             capitalized_name = capitalized_name + letter.upper()
+#         else:
+#             capitalized_name = capitalized_name + letter
+#         prevLetter = letter
+#         firstLetter = False
+#         # if letter == " ":
+#         #     full_name[letter + 1] = full_name[letter + 1].upper()
+#     # full_name_split = full_name.split()
+#     # for name in full_name_split:
+#     #     name = name.capitalize()
+#     return capitalized_name
+
+#namn = input("Mata in namn: ")
+# def capitalizeNames(full_name):
+#     name_split = full_name.split()
+#     result = ""
+#     for name in name_split:
+#         name = name.capitalize()
+#         result = result + name + " "
+    
+#     print(result)
+#     #Count and remove all but the last space
+#     space_count = result.count(" ")
+#     result = result.replace(" ", "", space_count-1)
+#     return result
+
+# namn = "steven"
+# namn = namn.capitalize()
+# print(namn)
+
+# #steven lomon lennartsson
+# namn = input("Mata in namn: ")
+# def capitalizeNames(full_name):
+#     name_split = full_name.split()
+#     for index in range(len(name_split)):
+#         name_split[index] = name_split[index].capitalize()
+#     result = ' '.join(name_split)
+#     return result
 
 # print(f"Before: {namn}")
 # namn_capitalized = capitalizeNames(namn)
 # print(f"After: {namn_capitalized}")
 
 # text = "Detta är en sträng som du skall ändra"
-# spaceCounter = 0
-# for s in text:
-#     if s == " ":
-#         spaceCounter += 1
+# spaceCounter = text.count(" ")
+# # for s in text:
+# #     if s == " ":
+# #         spaceCounter += 1
 # text_space_replaced = text.replace(" ", "*")
 
 # print(f"Before: {text}")
@@ -153,31 +190,96 @@ print(board)
 
 # mail = input("Mata in en mailadress: ")
 # def mailControl(input_mail):
-#     length = len(input_mail)
-#     a = False
-#     b = False
-#     atCounter = 0
-#     dotCounter = 0
-#     dotPosition = 0
+#     length = len(input_mail) - 1
+#     atCounter = input_mail.count("@")
+#     lastDotPosition = input_mail.rfind(".")
 #     dotBool = False
-#     for x in input_mail:
-#         if x == "@":
-#             atCounter += 1
-#             a = True
-#         if x == ".":
-#             dotCounter += 1
-#             b = True
-#     for x in input_mail:
-#         dotPosition += 1
-#         if x == ".":
-#             break
-#     if length - dotPosition == 2 or length - dotPosition == 3:
+#     if length - lastDotPosition == 2 or length - lastDotPosition == 3:
 #         dotBool = True
-#     print(f"Length = {length}")
-#     print(f"dotPosition = {dotPosition}")
-#     if a and b and atCounter == 1 and dotCounter >= 1 and dotBool:
+#     if atCounter == 1 and dotBool:
 #         return True
 #     else:
 #         return False
 
-# print(mailControl(mail))
+# if mailControl(mail):
+#     print("Du har matat in en giltig e-mail")
+# else:
+#     print("Du har matat in en ogiltig e-mail")
+
+# text = "Detta är min text som jag matar in"
+# #text = input("Mata in en text: ")
+# def countWords(text):
+#     text_split = text.split()
+#     return len(text_split) #return size of the list
+
+# print(f"Texten innehåller {countWords(text)} ord")
+
+# maybePalindrome = input("Mata in text för att checka palindrom: ")
+# def isPalindrome(input):
+#     input_spaces_removed = input.replace(" ", "")
+#     indexLength = len(input_spaces_removed) - 1
+#     input_reversed = ""
+#     for index in range(indexLength,-1,-1):
+#         input_reversed = input_reversed + input_spaces_removed[index]
+#     if input_reversed == input_spaces_removed:
+#         return True
+#     else:
+#         return False
+
+# if isPalindrome(maybePalindrome):
+#     print(f"{maybePalindrome} is a palindrome!")
+# if not isPalindrome(maybePalindrome):
+#     print(f"{maybePalindrome} is a not palindrome")
+
+#LECTURE
+# while True: #THIS IS A SUPER USEFUL INFINITE LOOP, WILL BE USED A LOT!!
+#     print("Spelet spelas")
+#     inmatning = input("Vill du spela igen Ja/Nej? ").lower()
+#     if inmatning == "nej":
+#         break
+#     print("Vi avslutar...")
+
+# while True:
+#     inmatning = input(":")
+#     #if inmatning.find("jäkla") != -1: #THIS LINE IS NOT USED ANYMORE
+#         #if we can't find the position of a char, the position is -1
+#     inmatning = inmatning.replace("jäkla", "*****")
+#     print(inmatning)
+
+# namn = "Steven Lomon Lennartsson"
+# length = len(namn)
+# for index in range(length):
+#     print(namn[index])
+
+# print(namn[-1])
+
+# allAccounts = ["1212", "3311", "9999"]
+# while True:
+#     print("1. Lista alla konton")
+#     print("2. Skriv ut hur många konton")
+#     print("3. Lägg till konton")
+#     print("4. Ta bort konton")
+#     print("5. Söka")
+#     action = input("Ange val:")
+#     if action == "2":
+#         print(f"Antal konton:{len(allAccounts)}")
+#     elif action == "1":
+#         for namn in allAccounts:
+#             print(namn)
+#     elif action == "3":
+#         namn = input("Ange namn på nya kontot")
+#         allAccounts.append(namn)
+#     elif action == "4":
+#         for index in range(0,len(allAccounts)):
+#             print(f"{index+1} {allAccounts[index]}")
+        
+#         index = input("Ange kontonummer på kontot som ska tas bort: ")
+#         index = index = index - 1
+#         del allAccounts[index]
+#     elif action == "5":
+#         searchQuery = input("Sök efter:").lower()
+#         for namn in allAccounts:
+#             lowerCaseNamn = namn.lower()
+#             index = lowerCaseNamn.find(searchQuery)
+#             if index != -1:
+#                 print(namn)
--- Vecka3.py
+++ Vecka3.py
@@ -3,10 +3,32 @@
 #DICTIONARIES
 def dictKeyCheck(dict, key):
     if key in dict:
-        print("Key is present in the dictionary")
+        return "Key is present in the dictionary"
     else:
-        print("Key is not present in the dictionary")
+        return "Key is not present in the dictionary"
 
 d = {1: 15, 2: 18, 3: 22}
-dictKeyCheck(d,2)
-dictKeyCheck(d,4)
+print(f"For 2 in d: {dictKeyCheck(d,2)}")
+print(f"For 4 in d: {dictKeyCheck(d,4)}")
+
+def dictPrintUnique(dict):
+    uniqueDict = dict.copy()
+    return uniqueDict
+
+s =  [{"V":"S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII":"S005"}, {"V":"S009"},
+{"VIII":"S007"}]
+print(f"Unique values: {dictPrintUnique(s)}")
+
+def dictGetTopThree(dict):
+    dictKeys = list(dict.keys())
+    dictValues = list(dict.values())
+    dictValues.sort()
+    topThreeValues = [dictValues[-3:]] #USEFUL! Get the last three items in the sorted list (the three largest items)
+    #topThreeItems = [
+    #    [dict[] == topThreeValues[-1], topThreeValues[-1]]
+    #]
+
+    return topThreeValues
+
+sample = {'item1': 45.50, 'item2':35, 'item3': 41.30, 'item4':55, 'item5': 24}
+print(f"Top three items in shop: {dictGetTopThree(sample)}")
