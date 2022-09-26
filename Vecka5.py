08fc0622b8252c2f70d6f7c758b676b19903f021
Steven Lomon Lennartsson <steven.lennartsson@gmail.com>
2022-09-26 17:44:40 +0200

    All Datetime exercises done

--- Vecka5.py
+++ Vecka5.py
@@ -1,41 +1,90 @@
 #YH Grundläggande Python Vecka 5 labbar
 from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 
-print("\nPRINT DATE IN TWO FORMATS")
+def getWeekdayName(weekdaynum:int) -> str:
+    if weekdaynum == 0:
+        return "Monday"
+    if weekdaynum == 1:
+        return "Tuesday"
+    if weekdaynum == 2:
+        return "Wednesday"
+    if weekdaynum == 3:
+        return "Thursday"
+    if weekdaynum == 4:
+        return "Friday"
+    if weekdaynum == 5:
+        return "Saturday"
+    if weekdaynum == 6:
+        return "Sunday"
+    return "Invalid day"
+
+def convertToDatetime(date: str) -> datetime: #from format yyyy-mm-dd (Can we add this?)
+    year = int(date[0:4])
+    mon = int(date[5:7])
+    day = int(date[8:10])
+    convDate = datetime(year, mon, day)
+    return convDate
+
+def getDate(prompt = "Enter a date on the format yyyy-mm-dd, or 'today' for today's date: ") -> str:
+    while True:
+        try:
+            date = input(prompt)
+            if date.lower() == 'today':
+                return datetime.now()
+            else:
+                if len(date) != 10:
+                    print("Invalid input")
+                else:
+                    break
+        except:
+            print("Invalid input")
+    return convertToDatetime(date)
+
+print("\n**PRINT DATE IN TWO FORMATS**")
 nu = datetime.now()
 print(f"Complete Date: {nu.strftime('%Y-%m-%d %H:%M:%S')}")
 print(f"Short Date: {nu.strftime('%Y-%m-%d')}")
 
-print("\nPARTS OF DATETIME")
+print("\n**PARTS OF DATETIME**")
 nu = datetime.now()
 print(f"Year = {nu.year}")
 print(f"Month = {nu.month}")
-print("etc etc")
+print("...")
+print(f"Second = {nu.second}")
+print(f"Millisecond = {nu.microsecond}")
 
-print("\nDAY OF THE WEEK FROM A DATE")
-def getWeekdayName(weekdaynum:int) -> str:
-    if weekdaynum == 0:
-        return "Måndag"
-    if weekdaynum == 1:
-        return "Tisdag"
-    if weekdaynum == 2:
-        return "Onsdag"
-    if weekdaynum == 3:
-        return "Torsdag"
-    if weekdaynum == 4:
-        return "Fredag"
-    if weekdaynum == 5:
-        return "Lördag"
-    if weekdaynum == 6:
-        return "Söndag"
-    return "Ogiltig dag"
+# print("\n**DAY OF THE WEEK FROM A DATE**")
+# year = int(input("Enter a year: "))
+# mon = int(input("Enter a month: "))
+# dag = int(input("Enter a day: "))
+# date = datetime(year, mon, dag)
+# weekday = getWeekdayName(date.weekday())
+# print(f"{date.strftime('%Y-%m-%d')} is a {weekday}")
 
-year = int(input("Ange ett år: "))
-mon = int(input("Ange en månad: "))
-dag = int(input("Ange en dag: "))
-date = datetime(input(year, mon, dag))
-weekday = getWeekdayName(date.weekday())
-print(f"Datumet du matat in är en {weekday}")
+# print("\n**ADD n DAYS**")
+# now = datetime.now()
+# print(f"Today = {now.strftime('%Y-%m-%d %H:%M:%S')}")
+# n = int(input("Enter amount of days to add: "))
+# diff = now + timedelta(days=n)
+# print(f"{n} days from now is {getWeekdayName(diff.weekday())}")
+
+# print("\n**INPUT PARSING**")
+# date = getDate()
+# print(date.strftime('%Y-%m-%d'))
+
+# print("\n**DAY DIFF BETWEEN TWO DATES**")
+# date1 = getDate("Enter first date on the format yyyy-mm-dd, or 'today' for today's date: ")
+# date2 = getDate("Enter second date on the format yyyy-mm-dd, or 'today' for today's date: ")
+# diff = abs(date1 - date2)
+# print(f"Difference in days: {diff.days}")
+
+print("\n**NUMBER OF DAYS IN A MONTH**")
+year = int(input("Enter year: "))
+month = int(input("Enter month: "))
+date = datetime(year, month, 1)
+newDate = date + relativedelta(months=+1) + timedelta(days=-1)
+print(f"Month {month} year {year} conains {newDate.day} days")
 
 #LECTURE
 # fakturaDatum = datetime.now()
